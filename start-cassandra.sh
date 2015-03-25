b6454840b610f792cc00cbe1668b9c2c48586535
Neale Swinnerton <neale@isismanor.com>
2015-03-25 13:54:09 +0000

    Make listen address(es) configurable.

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+CONFIG_FILE=/apache-cassandra/conf/cassandra.yaml
+
+LISTEN_ADDRESS=${CASSANDRA_LISTEN_ADDRESS?NOT DEFINED}
+
 # Unclear to me whether this is an issue with the datastax env code, but
 # for the version of jdk we're using 228k is the minimum accepted stack size,
 # tweak the config
-#q
 # TODO investigate the reason for this.
+
 sed -i -e "s@-Xss180k@-Xss228k@" /apache-cassandra/conf/cassandra-env.sh
 
-echo -e "data_file_directories:\n  -   /data/cassandra/${HOSTNAME}/" >> /apache-cassandra/conf/cassandra.yaml
+sed -i \
+    -e "s@localhost@${LISTEN_ADDRESS}@" \
+    -e "s@127\.0\.0\.1@${LISTEN_ADDRESS}@g" ${CONFIG_FILE}
+
+echo -e "data_file_directories:\n  -   /data/cassandra/${HOSTNAME}/" >> ${CONFIG_FILE}
+
 
 /apache-cassandra/bin/cassandra -f
