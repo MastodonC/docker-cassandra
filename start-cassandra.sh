3838cda0250e82ac2584ee2154d0b53723bd6b50
Neale Swinnerton <neale@isismanor.com>
2015-04-02 11:08:02 +0100

    better DATA_DIR and hostname handling.

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -2,7 +2,36 @@
 
 CONFIG_FILE=/apache-cassandra/conf/cassandra.yaml
 
-LISTEN_ADDRESS=${CASSANDRA_LISTEN_ADDRESS?NOT DEFINED}
+LISTEN_ADDRESS=${CASSANDRA_LISTEN_ADDRESS:-$(hostname -i)}
+
+
+# tabs not allowed
+YML_INDENT="    "
+
+DIR_TAIL="cassandra/${HOSTNAME}"
+LOGS_DIR="/logs/${DIR_TAIL}"
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
+if [ -z "${DATA_DIR_PATTERN}" ] ; then
+    DATA_DIRS="/data/${DIR_TAIL}"
+    mkdir -p ${DATA_DIRS}
+else
+    ddirs=()
+    candidates=(${DATA_DIR_PATTERN})
+    for dir in ${candidates[@]}
+    do
+	thedir="$(pwd)${dir}/${DIR_TAIL}"
+	mkdir -p ${thedir}
+	ddirs+=("${thedir}")
+    done
+    DATA_DIRS=$(join "\n${YML_INDENT}-" ${ddirs[@]})
+fi
+
+echo "CASSANDRA_LISTEN_ADDRESS is ${CASSANDRA_LISTEN_ADDRESS}"
+echo "DATA_DIRS is ${DATA_DIRS}"
+
+mkdir -p "${LOGS_DIR}"
 
 # Unclear to me whether this is an issue with the datastax env code, but
 # for the version of jdk we're using 228k is the minimum accepted stack size,
@@ -15,7 +44,6 @@ sed -i \
     -e "s@localhost@${LISTEN_ADDRESS}@" \
     -e "s@127\.0\.0\.1@${LISTEN_ADDRESS}@g" ${CONFIG_FILE}
 
-echo -e "data_file_directories:\n  -   /data/cassandra/${HOSTNAME}/" >> ${CONFIG_FILE}
-
+echo -e "data_file_directories:\n${YML_INDENT}- ${DATA_DIRS}" >> ${CONFIG_FILE}
 
 /apache-cassandra/bin/cassandra -f
